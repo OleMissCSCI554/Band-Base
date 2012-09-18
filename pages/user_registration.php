ff65fee9f28e9c651705c7f27a439e21980afca5
Giwoo Lee <gglee@go.olemiss.edu>
2012-09-18 13:22:04 -0500

    Add draft for 'user registration' page (Slide 5)
    Developed using PHP.
    - Background color: #000;
    - Body("content") color: #f4f4f5;
    - Width: 850px; Height: 480px
    - Box color: #a0b099

---
+++ pages/user_registration.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>User Registration</title>
+	<link rel="stylesheet" href="includes/style.css" type="text/css" media="screen" />
+</head>
+
+<body>
+	
+    <!-- Header -->
+    <div id="header"> 
+        <img src="images/Logo_resized.PNG" />       
+    </div>
+
+
+	<!-- Content -->
+    <div id="content">
+    	<div class="box"> 
+        	<div class="text">
+            	<p>Location</p>
+            	<p>Influences</p>
+                <p>Influences</p>
+            </div>
+            
+            <div class="input">
+            	<input type="text" name="location" size="18" />
+            	<input type="text" name="influences" size="18" />          
+            	<input type="text" name="influences2" size="18" />
+            </div>
+
+        </div>
+    </div>
+    
+    
+    <!-- Footer -->
+    <div id="footer">
+    
+    </div>
+
+</body>
+</html>
