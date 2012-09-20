6d926e1df4074ac15417d32650a8c360a0725e7a
William Panlener <wepanlen@go.olemiss.edu>
2012-09-19 19:13:08 -0500

    Cleaned up user registration page: whitespace, comments, and img alt tags

--- pages/user_registration.php
+++ pages/user_registration.php
@@ -1,42 +1,28 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<title>User Registration</title>
-	<link rel="stylesheet" href="../static/css/user-registration.css" type="text/css" />
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>User Registration</title>
+  <link rel="stylesheet" href="../static/css/user-registration.css" type="text/css" />
 </head>
-
 <body>
-	
-    <!-- Header -->
-    <div id="header"> 
-        <img src="../static/img/logo.png" />       
+  <div id="header">
+    <img src="../static/img/logo.png" alt="BandBase logo" />
+  </div>
+  <div id="content">
+    <div class="box">
+      <div class="text">
+        <p>Location</p>
+        <p>Influences</p>
+        <p>Influences</p>
+      </div>
+      <div class="input">
+        <input type="text" name="location" size="18" />
+        <input type="text" name="influences" size="18" />
+        <input type="text" name="influences2" size="18" />
+      </div>
     </div>
-
-
-	<!-- Content -->
-    <div id="content">
-    	<div class="box"> 
-        	<div class="text">
-            	<p>Location</p>
-            	<p>Influences</p>
-                <p>Influences</p>
-            </div>
-            
-            <div class="input">
-            	<input type="text" name="location" size="18" />
-            	<input type="text" name="influences" size="18" />          
-            	<input type="text" name="influences2" size="18" />
-            </div>
-
-        </div>
-    </div>
-    
-    
-    <!-- Footer -->
-    <div id="footer">
-    
-    </div>
-
+  </div>
+  <div id="footer"></div>
 </body>
 </html>
