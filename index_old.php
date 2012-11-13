53ea8a6cd737a894a26944db94ad11c37375e704
Jason M. Boyd <jmboyd1@turing.cs.olemiss.edu>
2012-11-13 14:00:58 -0600

    adding index page

---
+++ index_old.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>It works!</title>
+</head>
+<body>
+  <p>Apache works!</p>
+  <p><? include("/var/www.config/dbconfig.php"); ?></p>
+
+  <ul>
+    <li>url: <a style='text-decoration: line-through;' href='http://cs.olemiss.edu/bandbase'>http://cs.olemiss.edu/bandbase</a> or <a href='http://wic.cs.olemiss.edu'>http://wic.cs.olemiss.edu</a> or <a href='http://130.74.96.71'>http://130.74.96.71</a> There is a network configuration issue with the first url. Use one of the other two for now.</li>
+    <li>boilerplate: <a href='http://wic.cs.olemiss.edu/pages/'>http://wic.cs.olemiss.edu/pages/</a></li>
+    <li>phpmyadmin: <a href='http://wic.cs.olemiss.edu/phpmyadmin'>http://wic.cs.olemiss.edu/phpmyadmin</a></li>
+    <li>git: <a href='https://github.com/OleMissCSCI554/Band-Base.git'>https://github.com/OleMissCSCI554/Band-Base.git</a></li>
+    <li>git markdown writeup: <a href='http://github.github.com/github-flavored-markdown'>http://github.github.com/github-flavored-markdown</a></li>
+    <li>irc: <a href='irc://irc.freenode.net/bandbase'>irc://irc.freenode.net/bandbase</a></li>
+    <li>sysadmin: William Panlener wepanlen@go.olemiss.edu</li>
+    <li>dbadmin: Alex Movitz almovitz@go.olemiss.edu</li>
+  </ul>
+</body>
+</html>
